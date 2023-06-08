5d0a6943e1bb2b76e5598670943b25be71f1aa59
ML-Laurane <laurane.mouronval@gmail.com>
2023-06-08 08:52:59 +0200

    nouvelle version

--- src/lr/LR.java
+++ src/lr/LR.java
@@ -14,7 +14,7 @@ import java.util.concurrent.ExecutorService;
 class LR {
 	static final int LARGEUR = 1980;
 	static final int HAUTEUR = 1080;
-	static final int NBRAYONS = 300;
+	static final int NBRAYONS = 10;
 	static final int NIVEAU = 2;
 	
 	// valeurs originelles : 
@@ -23,39 +23,48 @@ class LR {
 	// static final int NBRAYONS = 10;
 	// static final int NIVEAU = 2;
 	public static void main(String[] args) {
+		// Début du temps d'éxecution (optionnel)
 		long start = System.nanoTime();
+
+		// Initialisation des variables
 		int nb_threads = 10;
 		Thread[] threads = new Thread[nb_threads];
 		Renderer r = new Renderer(LARGEUR, HAUTEUR);
-		Scene sc = new FormatSimple().charger("simple.txt");
+		Scene sc = new FormatSimple().charger(args[0]);
 		sc.display();
 		r.setScene(sc);
 		r.setNiveau(NIVEAU);
 
-		// r.renderFullImage(NBRAYONS);
+		// Création d'un pool de Threads pour optimiser
 		ExecutorService espool = Executors.newFixedThreadPool(nb_threads);
 		for (int i = 0; i < threads.length; i++) {
+			// Division des lignes de l'image entre les différents Threads
 			ParallelRenderer pr = new ParallelRenderer(i * (HAUTEUR / threads.length),
 					i * (HAUTEUR / threads.length) + (HAUTEUR / threads.length), NBRAYONS, r);
 			Thread t = new Thread(pr);
-			espool.submit(()->t.start());
+			espool.submit(() -> t.start());
 			threads[i] = t;
-		 	// t.start();
+			// t.start();
 		}
 
-		 for (int i = 0; i < threads.length; i++) {
-		 	try {
-		 		threads[i].join();
-		 	} catch (Exception e) {
-		 		System.out.println(e);
-		 	}
-		 }
+		// Attente de la fin des threads
+		for (int i = 0; i < threads.length; i++) {
+			try {
+				threads[i].join();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
 
+		// Fermerture du pool de Threads (impoortant sinon le programme ne se finit pas)
 		espool.shutdown();
+		// Rendu de l'image
 		Image image = r.getIm();
 
+		//Sauvegarde de l'image
 		image.save("image" + NIVEAU, "png");
-		//new MaterialFormat().charger("chaise_plan.mtl");
+
+		//Calcul du temps d'éxecution (optionnel)
 		System.out.println("Temps : " + (System.nanoTime() - start));
 	}
 }
--- src/lr/ParallelRenderer.java
+++ src/lr/ParallelRenderer.java
@@ -1,32 +1,44 @@
 package lr;
 
-public class ParallelRenderer implements Runnable{
+public class ParallelRenderer implements Runnable {
+    // Variables de la classe
+    // Les lignes minimum et maximum pour savoir quelles lignes calculer
     private int min, max;
+    // Le nombre de rayons à calculer
     private int nb_rayons;
+    // L'image pour laquelle on va calculer les lignes
     private Renderer rend;
 
+    // Constructeur de l'Objet pour initialiser les variables
     public ParallelRenderer(int min, int max, int nbrayons, Renderer r) {
         this.min = min;
         this.max = max;
         this.nb_rayons = nbrayons;
         this.rend = r;
     }
+
+    // Méthode pour calculer les lignes de l'image entre la ligne min et la ligne max
     public void run(){
         for (int i = this.min; i < this.max; i++) {
             rend.renderLine(i, this.nb_rayons);
         }
     }
 
-    // getter / setter 
+    // Min getter
     public int getMin(){
         return this.min;
     }
-    public int getMax(){
+    // Max getter
+    public int getMax() {
         return this.max;
     }
-    public void setMin(int min){
+    
+    // Min setter
+    public void setMin(int min) {
         this.min = min;
     }
+    
+    // Max setter
     public void setMax(int max){
         this.max = max;
     }
